6b8397671f5557eff7057544c5eff3dffcee7b71
alanguilhermeM <alanguilherme13@outlook.com>
2023-11-30 06:18:37 -0300

    Finalizado requisito 4

--- tests/actions/test_show_details.py
+++ tests/actions/test_show_details.py
@@ -3,18 +3,56 @@ from pro_filer.actions.main_actions import show_details  # NOQA
 
 def test_show_details(capsys):
     context = {
-        "base_path": "/home/trybe/Downloads/Trybe_logo.png"
+        "base_path": "pro_filer/__init__.py"
     }
 
     show_details(context)
 
     captured = capsys.readouterr()
     expected_output = (
-        "File name: Trybe_logo.png\n"
-        "File size in bytes: 22438\n"
+        "File name: __init__.py\n"
+        "File size in bytes: 0\n"
         "File type: file\n"
-        "File extension: .png\n"
-        "Last modified date: 2023-06-13\n"
+        "File extension: .py\n"
+        "Last modified date: 2023-11-28\n"
     )
 
     assert captured.out == expected_output
+
+
+def test_show_details_with_no_extension(capsys):
+    context = {
+        "base_path": "pro_filer"
+    }
+
+    show_details(context)
+
+    captured = capsys.readouterr()
+    expected_output = (
+        "File name: pro_filer\n"
+        "File size in bytes: 4096\n"
+        "File type: directory\n"
+        "File extension: [no extension]\n"
+        "Last modified date: 2023-11-28\n"
+    )
+
+    assert captured.out == expected_output
+
+
+def test_not_existing_show_details(capsys):
+    context = {
+        "base_path": "/home/trybe/????"
+    }
+
+    show_details(context)
+
+    captured = capsys.readouterr()
+    # expected_output = (
+    #     "File name: Trybe_logo.png\n"
+    #     "File size in bytes: 22438\n"
+    #     "File type: file\n"
+    #     "File extension: .png\n"
+    #     "Last modified date: 2023-06-13\n"
+    # )
+
+    assert captured.out == "File '????' does not exist\n"
